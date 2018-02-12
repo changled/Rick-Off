84d231951dc57f6d5639469d3ba7c00dcc697076
Rachel Chang <Rachel@Rachels-MacBook-Pro.local>
2018-02-12 11:55:38 -0800

    implemented turn-based tower shooting

--- Rick-Off 2D/Assets/Scripts/TowerController.cs
+++ Rick-Off 2D/Assets/Scripts/TowerController.cs
@@ -6,48 +6,73 @@ using UnityEngine.UI;
 public class TowerController : MonoBehaviour {
 	public float speed;
 	public Text axisText;
+	public Text testingText;
 	private Rigidbody2D tankHeadRB;
 	public GameObject tankHead;
-//	public float fireRate = 0;
-//	public float damage = 10;
-//	public LayerMask notToHit;
-//	private float timeToFire = 0;
-//	private Transform firePoint;
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn1;
+	public Transform bulletSpawn2;
+	public float fireRate = 5;
+	public float damage = 10;
+	public LayerMask whatToHit;
+	private float timeToFire = 0;
+	private Transform firePoint;
+	public Vector2 pivotPoint;
+	public Vector2 firePointPosition;
+	private Vector2 shootPoint1;
+	private Vector2 shootPoint2;
 
-//	void Awake() {
-//		firePoint = transform.Find ("FirePoint");
-//
-//		if (firePoint == null) {
-//			Debug.LogError ("No firepoint!");
-//		}
-//	}
+	void Awake() {
+		
+		firePoint = transform.Find ("FirePoint");
+
+		if (firePoint == null) {
+			Debug.LogError ("No firepoint!");
+		}
+		//		Debug.DrawLine(firePointPosition, fireDirection * 10);
+	}
 
 	void Start() {
 		tankHeadRB = tankHead.GetComponent<Rigidbody2D>();
 		speed = 50;
-		axisText.text = "on start";
+		testingText.text = "Nothing so far";
 	}
 
-//	void Update() {
-//		if (fireRate == 0) {
-//			if (Input.GetButtonDown ("Fire1")) {
-//				Shoot ();
-//			}
-//		} //if holding down fire button
-//		else if(Input.GetButton("Fire1") && Time.time > timeToFire) {
-//			timeToFire = Time.time + 1 / fireRate;
-//			Shoot ();
-//		}
-//	}
+	void Update() {
+
+		if (fireRate == 0) {
+			if (Input.GetButtonDown ("Fire1")) {
+				Fire();
+			}
+		} //if holding down fire button
+		else if(Input.GetButton("Fire1") && Time.time > timeToFire) {
+			timeToFire = Time.time + 1 / fireRate;
+			Fire();
+		}
+	}
 
 	void FixedUpdate() {
-		float turnAmount = Input.GetAxis ("Horizontal");
-		tankHeadRB.MoveRotation (tankHeadRB.rotation - turnAmount * speed * Time.fixedDeltaTime);
+		float turnAmount = Input.GetAxis ("Horizontal") * speed * Time.fixedDeltaTime;
+		tankHeadRB.MoveRotation (tankHeadRB.rotation - turnAmount);
 
 		axisText.text = "current rotation: " + tankHeadRB.rotation.ToString() + "\nhorizontal turn amount: " + turnAmount.ToString ();
 	}
 
-	void Shoot() {
-		Debug.LogError ("Fire!");
+	void Fire() {
+		var bullet1 = (GameObject)Instantiate (bulletPrefab, bulletSpawn1.position, bulletSpawn1.rotation);
+		var bullet2 = (GameObject)Instantiate (bulletPrefab, bulletSpawn2.position, bulletSpawn2.rotation);
+		Vector2 leftGunDir = bullet1.transform.position - tankHead.transform.position;
+		Vector2 rightGunDir = bullet2.transform.position - tankHead.transform.position;
+		bullet1.GetComponent<Rigidbody2D> ().velocity = (leftGunDir * 10);
+		bullet2.GetComponent<Rigidbody2D> ().velocity = (rightGunDir * 10);
+
+//		Debug.DrawLine(pivotPoint, bullet.transform.position);
+//		Debug.Log ("FIRE!");
+		Destroy (bullet1, 2.0f); //destroy bullet ImageEffectAfterScale 2 seconds
+		Destroy (bullet2, 2.0f); //destroy bullet ImageEffectAfterScale 2 seconds
 	}
+
+//	function OnBecameInvisible() {
+//		Destroy(GameObject);
+//	}
 }
